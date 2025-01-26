e537a19d6a9cf759168c527561260aad2366f461
mandu <jys0972@gmail.com>
2025-01-26 19:51:29 +0900

    refact(MapUtil.kt) : map 관련 로직을 Map Util로 이동

--- app/src/main/java/com/dudoji/android/map/MapActivity.kt
+++ app/src/main/java/com/dudoji/android/map/MapActivity.kt
@@ -1,98 +1,35 @@
 package com.dudoji.android.map
 
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.dudoji.android.R
+import com.dudoji.android.util.MapUtil
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.OnSuccessListener
 
 class MapActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
-    lateinit var providerClient: FusedLocationProviderClient
-    lateinit var apiClient: GoogleApiClient
+
     var googleMap: GoogleMap? = null
+    var mapUtil: MapUtil = MapUtil(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ){
-            if(it.all {permission -> permission.value == true}){
-                apiClient.connect()
-            }else{
-                Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment)!!.getMapAsync(this)
-
-        providerClient = LocationServices.getFusedLocationProviderClient(this)
-        apiClient = GoogleApiClient.Builder(this)
-            .addApi(LocationServices.API)
-            .addConnectionCallbacks(this)
-            .addOnConnectionFailedListener(this)
-            .build()
-
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-            !== PackageManager.PERMISSION_GRANTED){
-            requestPermissionLauncher.launch(
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
-            )
-        }else{
-            apiClient.connect()
-        }
+        mapUtil.requestLocationPermission()
+        mapUtil.prepareMap()
+        mapUtil.setupLocationServices()
     }
 
-    private fun moveMap(latitude: Double, longitude: Double){
-        val latLng = LatLng(latitude, longitude)
-        val position: CameraPosition = CameraPosition.Builder()
-            .target(latLng)
-            .zoom(16f)
-            .build()
-        googleMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(position))
-        val markerOption = MarkerOptions()
-        markerOption.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-        markerOption.position(latLng)
-        markerOption.title("MyLocation")
-        googleMap?.addMarker(markerOption)
-    }
+
+
 
     override fun onConnected(p01: Bundle?){
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-            === PackageManager.PERMISSION_GRANTED){
-            providerClient.lastLocation.addOnSuccessListener(
-                this@MapActivity,
-                object : OnSuccessListener<Location>{
-                    override fun onSuccess(p0: Location?) {
-                        p0?.let{
-                            val latitude = p0.latitude
-                            val longitude = p0.longitude
-                            moveMap(latitude, longitude)
-                        }
-                    }
-                }
-            )
-            apiClient.disconnect()
-        }
+        mapUtil.moveMapToCurrentLocation(googleMap)
     }
 
     override fun onConnectionSuspended(p0: Int) {
