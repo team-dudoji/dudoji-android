6d6cbfb410eddeafc6c1b365e956045ffad1a8bb
mandu <jys0972@gmail.com>
2025-01-26 19:32:11 +0900

    feat(MainActivity.kt) : map activity로 이동하는 button 등록

--- app/src/main/java/com/dudoji/android/MainActivity.kt
+++ app/src/main/java/com/dudoji/android/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dudoji.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.dudoji.android.map.MapActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setButtonMapping()
+    }
+
+    fun setButtonMapping() {
+        var toMapButton = findViewById<Button>(R.id.toMapButton)
+
+        toMapButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                var intent = Intent(this@MainActivity, MapActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
 }
