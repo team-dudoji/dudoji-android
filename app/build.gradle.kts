9123a89e433a7503bc690c0eb9740459ff44a450
mandu <jys0972@gmail.com>
2025-02-01 13:12:47 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     implementation("com.google.android.gms:play-services:12.0.1")
 }
