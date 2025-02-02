3f76ec62120afc89006056f8703449004915e384
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-02-02 16:52:51 +0900

    refact(Myservice.kt)
    포그라운드로 알림창에 띄우는 기능 부가적으로 넣었으나 작동이 안되서 일단 제거

--- app/src/main/java/com/dudoji/android/MyService.kt
+++ app/src/main/java/com/dudoji/android/MyService.kt
@@ -1,88 +1,17 @@
 package com.dudoji.android
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.Service
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.IBinder
 import android.util.Log
-import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MyService : Service() {
-    
-    //시작 변수 선언
-    companion object {
-        private const val NOTI_ID = 1
-    }
-
-    //로그 찍기
-    private val scope = CoroutineScope(Dispatchers.Default)
-
-    fun startCounting() {
-        scope.launch {
-            repeat(100) { i ->
-                Log.d(TAG, "count: $i")
-                delay(1000)
-            }
-        }
-    }
-
-    fun stopCounting() {
-        scope.cancel()
-    }
-
-
-    private fun createNotification() {
-        val builder = NotificationCompat.Builder(this, "default")
-
-        builder.apply {
-            setSmallIcon(R.mipmap.ic_launcher)
-            setContentTitle("Foreground Service")
-            setContentText("포그라운드 서비스")
-            color = Color.RED
-        }
-
-        val notificationIntent = Intent(this, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
-        }
-
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            notificationIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-
-        builder.setContentIntent(pendingIntent)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManager.createNotificationChannel(
-                NotificationChannel(
-                    "default",
-                    "기본 채널",
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-            )
-        }
-
-
-        val notification = builder.build()
-        notificationManager.notify(NOTI_ID, notification)
-        startForeground(NOTI_ID, notification)
-    }
 
     private var job: Job? = null
 
@@ -100,7 +29,6 @@ class MyService : Service() {
         job?.cancel()
         super.onDestroy()
     }
-
 
 
     override fun onBind(intent: Intent): IBinder {
