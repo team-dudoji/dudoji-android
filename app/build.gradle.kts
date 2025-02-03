ec8db513415fa9f6f965c83fb8966b95085e2d24
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-02-03 23:01:53 +0900

    refaactor(build.gradle)
    데베 연결땜시 버전 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.mediation.test.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.android.gms:play-services:12.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    // Maps SDK for Android
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    // If using Places API
+    implementation("com.google.android.libraries.places:places:3.2.0")
+
+    // Google API Client
+    implementation("com.google.api-client:google-api-client-android:2.0.0")
 }
