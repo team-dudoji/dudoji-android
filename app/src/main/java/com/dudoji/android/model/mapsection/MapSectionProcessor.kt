d0a23b7f7641097cd2f5d2e67f894a0ede9c4f36
mandu <jys0972@gmail.com>
2025-02-09 21:20:31 +0900

    feat(MapSectionProcessor.kt) : explored rate를 통해서 bit를 계산하는 함수 구현

--- app/src/main/java/com/dudoji/android/model/mapsection/MapSectionProcessor.kt
+++ app/src/main/java/com/dudoji/android/model/mapsection/MapSectionProcessor.kt
@@ -4,8 +4,8 @@ class MapSectionProcessor(private val mapSections: List<MapSection>) {
 
     companion object {
         const val DETAIL_PROCESSING_RATE = 2
+        const val EXPLORED_RATE_THRESHOLD = 0.5f
     }
-
 
     fun getBit(minLat: Double, minLng : Double, size: Double): Boolean {
         if ((size / MapSection.MAPSECTION_LATLNG_SIZE) < DETAIL_PROCESSING_RATE) {
@@ -15,14 +15,34 @@ class MapSectionProcessor(private val mapSections: List<MapSection>) {
         }
     }
 
+    // get bit in detail (by map section's bit map)
     private fun getBitDetail(minLat: Double, minLng : Double, size: Double): Boolean {
         // TODO: implement this function
         return false
     }
 
+    // get bit roughly (by map section's explored rate)
     private fun getBitRoughly(minLat: Double, minLng : Double, size: Double): Boolean {
-        // TODO: implement this function
-        return false
+        val maxLat = minLat + size
+        val maxLng = minLng + size
+
+        val minDudojiX = Math.floor((minLng - MapSection.BASE_LNG) / MapSection.MAPSECTION_LATLNG_SIZE).toInt()
+        val minDudojiY = Math.floor((minLat - MapSection.BASE_LAT) / MapSection.MAPSECTION_LATLNG_SIZE).toInt()
+        val maxDudojiX = Math.ceil((maxLng - MapSection.BASE_LNG) / MapSection.MAPSECTION_LATLNG_SIZE).toInt()
+        val maxDudojiY = Math.ceil((maxLat - MapSection.BASE_LAT) / MapSection.MAPSECTION_LATLNG_SIZE).toInt()
+
+        var totalExplorationRate = 0.0f
+        var numOfMapSections = (maxDudojiX - minDudojiX + 1) * (maxDudojiY - minDudojiY + 1)
+
+        for (dudojiX in minDudojiX until maxDudojiX) {
+            for (dudojiY in minDudojiY until maxDudojiY) {
+                val mapSection = getMapSection(dudojiX, dudojiY)
+                if (mapSection != null) {
+                    totalExplorationRate += mapSection.getExploredRate() / numOfMapSections
+                }
+            }
+        }
+        return totalExplorationRate > EXPLORED_RATE_THRESHOLD
     }
 
     // get map section by dudojiX and dudojiY
