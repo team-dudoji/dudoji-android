592cf6821a8116de7d54d000b0ca84d85aa96d54
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-02-10 09:00:30 +0900

    refactor(build.gradle.kts)
    필요없는 의존성 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,15 +63,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.android.gms:play-services:12.0.1")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
 
-    // Maps SDK for Android
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-
-    // If using Places API
-    implementation("com.google.android.libraries.places:places:3.2.0")
-
-    // Google API Client
-    implementation("com.google.api-client:google-api-client-android:2.0.0")
 }
