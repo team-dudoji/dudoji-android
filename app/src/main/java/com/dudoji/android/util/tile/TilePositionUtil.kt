8bb5b7faaf12d4bc9474ab90682bcd598d1e1f2b
mandu <jys0972@gmail.com>
2025-02-10 15:26:25 +0900

    feat(TilePositionUtil.kt) : tile size 함수 구현

--- app/src/main/java/com/dudoji/android/util/tile/TilePositionUtil.kt
+++ app/src/main/java/com/dudoji/android/util/tile/TilePositionUtil.kt
@@ -3,15 +3,19 @@ package com.dudoji.android.util.tile
 // for google tile system's positioning (by x, y, zoom)
 class TilePositionUtil {
     companion object {
-
         // Convert Google Map Tile Position to LatLng
         // (x, y), (x+1, y+1) rectangle is tile area
         fun tilePositionToLatLng(x: Int, y: Int, zoom: Int): Pair<Double, Double> {
-            val n = Math.pow(2.0, zoom.toDouble())
+            val n = getTileSize(zoom)
             val lng_deg = x / n * 360.0 - 180.0
             val lat_rad = Math.atan(Math.sinh(Math.PI * (1 - 2 * y / n)))
             val lat_deg = Math.toDegrees(lat_rad)
             return Pair(lat_deg, lng_deg)
+        }
+
+        // Get Tile Size by zoom level
+        fun getTileSize(zoom: Int): Double {
+            return Math.pow(2.0, zoom.toDouble())
         }
     }
 }
