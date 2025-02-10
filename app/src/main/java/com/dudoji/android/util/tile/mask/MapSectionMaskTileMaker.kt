f1155d8fa080ee1c4cf6d69c2f0b7740559c3c97
mandu <jys0972@gmail.com>
2025-02-10 17:11:10 +0900

    feat(MapSectionMaskTileMaker.kt) : coroutineScope를 사용해서 병렬 적으로 실행되도록 수정

--- app/src/main/java/com/dudoji/android/util/tile/mask/MapSectionMaskTileMaker.kt
+++ app/src/main/java/com/dudoji/android/util/tile/mask/MapSectionMaskTileMaker.kt
@@ -6,6 +6,12 @@ import android.util.Log
 import com.dudoji.android.model.mapsection.MapSectionProcessor
 import com.dudoji.android.util.tile.TILE_SIZE
 import com.dudoji.android.util.tile.TilePositionUtil
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class MapSectionMaskTileMaker(private val mapSectionProcessor: MapSectionProcessor): IMaskTileMaker {
 
@@ -16,18 +22,29 @@ class MapSectionMaskTileMaker(private val mapSectionProcessor: MapSectionProcess
 
         val bitmap = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888)
         var count = 0
-        for (i in 0 until TILE_SIZE) {
-            for (j in 0 until TILE_SIZE) {
-                val lat = minLatLng.first + i * fragmentSize
-                val lng = minLatLng.second + j * fragmentSize
-                val bitValue = !mapSectionProcessor.getBit(lat, lng, fragmentSize)
+        val mutex = Mutex()
+        val deferredList = mutableListOf<Deferred<Unit>>()
 
-                val color = if (bitValue) Color.BLACK else Color.WHITE
+        coroutineScope {
+            for (i in 0 until TILE_SIZE) {
+                for (j in 0 until TILE_SIZE) {
+                    deferredList.add(
+                        async {
+                            val lat = minLatLng.first + i * fragmentSize
+                            val lng = minLatLng.second + j * fragmentSize
+                            val bitValue = !mapSectionProcessor.getBit(lat, lng, fragmentSize)
 
+                            val color = if (bitValue) Color.BLACK else Color.WHITE
 
-                bitmap.setPixel(j, i, color)
-                count += 1
+                            mutex.withLock {
+                                count += 1
+                                bitmap.setPixel(j, i, color)
+                            }
+                        }
+                    )
+                }
             }
+            deferredList.awaitAll()
         }
 
         return bitmap
