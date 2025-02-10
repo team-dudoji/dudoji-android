24fc9f815ef999a41d7cbc44993d6e820d8854ff
mandu <jys0972@gmail.com>
2025-02-10 17:09:57 +0900

    feat(MaskTileProvider.kt) : createMaskTile을 suspend를 바꾸고 blocking하게 수정

--- app/src/main/java/com/dudoji/android/util/tile/MaskTileProvider.kt
+++ app/src/main/java/com/dudoji/android/util/tile/MaskTileProvider.kt
@@ -4,6 +4,7 @@ package com.dudoji.android.util.tile
 import android.graphics.Bitmap
 import com.dudoji.android.util.tile.mask.IMaskTileMaker
 import com.google.android.gms.maps.model.*
+import kotlinx.coroutines.runBlocking
 import java.io.ByteArrayOutputStream
 
 const val TILE_SIZE = 256
@@ -12,7 +13,7 @@ const val TILE_SIZE = 256
 class MaskTileProvider(private val maskTileMaker: IMaskTileMaker) : TileProvider {
 
     override fun getTile(x : Int, y : Int, zoom : Int) : Tile {
-        val bitmap : Bitmap = maskTileMaker.createMaskTile(x, y, zoom)
+        val bitmap : Bitmap = runBlocking { maskTileMaker.createMaskTile(x, y, zoom) }
         val stream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
         val byteArray = stream.toByteArray()
