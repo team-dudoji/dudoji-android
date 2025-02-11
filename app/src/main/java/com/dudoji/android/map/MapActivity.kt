d6fdb9d15f5b17ed8631fa697b6f9281abd6bf91
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-02-11 18:19:43 +0900

    refactor(MapActivity.kt)
    네비게이션바 추가

--- app/src/main/java/com/dudoji/android/map/MapActivity.kt
+++ app/src/main/java/com/dudoji/android/map/MapActivity.kt
@@ -1,14 +1,18 @@
 package com.dudoji.android.map
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dudoji.android.MainActivity
 import com.dudoji.android.R
 import com.dudoji.android.util.MapUtil
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var bottomNav: BottomNavigationView
 
     var googleMap: GoogleMap? = null
     var mapUtil: MapUtil = MapUtil(this)
@@ -20,10 +24,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapUtil.setupLocationServices()
         mapUtil.requestLocationPermission()
         mapUtil.prepareMap()
+
+        bottomNav = findViewById(R.id.navigationView)
+        setupBottomNavigation()
+
+        bottomNav.selectedItemId = R.id.mapFragment
     }
 
     override fun onMapReady(p0: GoogleMap?) {
         googleMap = p0
         mapUtil.setGoogleMap(p0)
     }
+
+    //맵에서 메인 엑티비티 가기
+    private fun setupBottomNavigation() {
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.mapFragment -> {
+                    // 맵 화면 유지
+                    true
+                }
+                R.id.homeFragment -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
 }
