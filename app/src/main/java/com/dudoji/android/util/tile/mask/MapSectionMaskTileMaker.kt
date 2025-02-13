04b7d3cee3b1a0cd5fbf4d4516db2e0c01887612
mandu <jys0972@gmail.com>
2025-02-13 17:11:00 +0900

    feat(MapSectionMask.TileMaker.kt) : mapSectionManager을 통해서 bitmap을 생성하도록 수정

--- app/src/main/java/com/dudoji/android/util/tile/mask/MapSectionMaskTileMaker.kt
+++ app/src/main/java/com/dudoji/android/util/tile/mask/MapSectionMaskTileMaker.kt
@@ -1,69 +1,12 @@
 package com.dudoji.android.util.tile.mask
 
 import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.util.Log
-import com.dudoji.android.model.mapsection.MapSectionProcessor
-import com.dudoji.android.util.tile.TILE_SIZE
-import com.dudoji.android.util.tile.TilePositionUtil
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import com.dudoji.android.model.mapsection.MapSectionManager
+import com.dudoji.android.model.mapsection.TileCoordinate
 
-const val TILE_PIXEL_SIZE = 2
 
-class MapSectionMaskTileMaker(private val mapSectionProcessor: MapSectionProcessor): IMaskTileMaker {
-
+class MapSectionMaskTileMaker(private val mapSectionManager: MapSectionManager): IMaskTileMaker {
     override suspend fun createMaskTile(x : Int, y : Int, zoom : Int): Bitmap {
-        val minLatLng = TilePositionUtil.tilePositionToLatLng(x, y, zoom)
-        val tileSize = TilePositionUtil.getTileSize(zoom)
-        val fragmentSize = tileSize / TILE_SIZE
-
-        val bitmap = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888)
-        var canvas = Canvas(bitmap)
-        var paint = Paint()
-
-        // apply black color with 50% transparency
-        paint.setColor(Color.RED)
-        canvas.drawRect(0F, 0F, TILE_SIZE.toFloat(), TILE_SIZE.toFloat(), paint)
-        var count = 0
-        val mutex = Mutex()
-        val deferredList = mutableListOf<Deferred<Unit>>()
-
-        coroutineScope {
-            for (i in 0 until (TILE_SIZE-10)/TILE_PIXEL_SIZE) {
-                for (j in 0 until (TILE_SIZE-10)/TILE_PIXEL_SIZE) {
-                    deferredList.add(
-                        async {
-                            val lat = minLatLng.first + i * TILE_PIXEL_SIZE * fragmentSize
-                            val lng = minLatLng.second + j * TILE_PIXEL_SIZE * fragmentSize
-                            val bitValue = !mapSectionProcessor.getBit(lat, lng, fragmentSize * TILE_PIXEL_SIZE)
-
-                            val color = if (bitValue) Color.BLACK else Color.YELLOW//Color.argb(128, 0, 0, 0)
-
-                            mutex.withLock {
-                                count += 1
-                                for (k in 0 until TILE_PIXEL_SIZE) {
-                                    for (l in 0 until TILE_PIXEL_SIZE) {
-                                        bitmap.setPixel(j * TILE_PIXEL_SIZE + k, i * TILE_PIXEL_SIZE + l, color)
-                                    }
-                                }
-                            }
-                        }
-                    )
-                }
-            }
-
-            Log.w("MaskTileMaker", "x: $x, y: $y, zoom: $zoom, size: ${TilePositionUtil.getTileSize(zoom)}")
-            Log.w("MaskTileMaker", "before calculating $count")
-            deferredList.awaitAll()
-            Log.w("MaskTileMaker", "after calculating $count")
-        }
-        return bitmap
+        return mapSectionManager.getBitmap(TileCoordinate(x, y, zoom))!!
     }
 }
