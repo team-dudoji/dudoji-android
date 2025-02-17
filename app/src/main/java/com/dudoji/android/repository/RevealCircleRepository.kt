455bd1577f826a6e93a61506738f67fed39cc246
mandu <jys0972@gmail.com>
2025-02-17 18:12:04 +0900

    feat(RevealCircleRepository.kt) : Location Repository의 이름 변경

---
+++ app/src/main/java/com/dudoji/android/repository/RevealCircleRepository.kt
@@ -0,0 +1,27 @@
+package com.dudoji.android.repository
+
+import android.location.Location
+import java.util.*
+
+data class RevealCircle(val lat: Double, val lng: Double, val radius: Double)
+
+const val MAX_LOG_SIZE = 20 //20개로 설정했으니 여기도 해줌
+
+object RevealCircleRepository {
+
+    val revealCircleQueue: Queue<RevealCircle> = LinkedList()
+
+    fun addLocation(location: Location) {
+        val revealCircle = RevealCircle(location.latitude, location.longitude, 100.0)
+        synchronized(this) {
+//            if (locationQueue.size >= MAX_LOG_SIZE) {
+//                locationQueue.poll()
+//            }
+            revealCircleQueue.add(revealCircle)
+        }
+    }
+
+    fun getLocations() : List<RevealCircle>{
+        return revealCircleQueue.toList()
+    }
+}
