7c332d86b09632cfefae091146279459070e9cc2
minseok <micheal117@pusan.ac.kr>
2025-02-17 17:33:43 +0900

    feature(NetWorkUtilTester.kt)
    네트워크 유틸 테스트를 위해 만듦

---
+++ app/src/androidTest/java/com/dudoji/android/util/NetworkUtilTester.kt
@@ -0,0 +1,12 @@
+package com.dudoji.android.util
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NetworkUtilTester {
+    fun RevealCircleTest(){
+
+    }
+
+}
