681bbc503aed31d161681d8684840f78c66dcba3
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-02-20 16:27:20 +0900

    refactor(LocationService.kt)
    로케이션 서비스에 콜백기능, 마지막 로케이션 리턴기능, 클라이언트 초기화 기능 추가

--- app/src/main/java/com/dudoji/android/util/location/LocationService.kt
+++ app/src/main/java/com/dudoji/android/util/location/LocationService.kt
@@ -1,41 +1,70 @@
 package com.dudoji.android.util.location
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Handler
 import android.os.Looper
-import android.widget.TextView
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.util.LinkedList
-import java.util.Queue
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 
-// Location을 지속적으로 받아오는 Class
-class LocationService {
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationCallback: LocationCallback
-    private val locationQueue: Queue<String> = LinkedList()
-    private lateinit var locationTextView: TextView
-    private lateinit var bottomNav: BottomNavigationView
-    private val handler = Handler(Looper.getMainLooper())
-    private val updateInterval = 3000L // 3초마다 갱신
+// Location을 지속적으로 받아오는 Class, 위치 추적 기능만 구현하므로 ui관련 변수 제거
+class LocationService { //위치 서비스 초기화 때문에 contxt를 private으로 저장
+    private var context: Context //외부에서 전달받은 Context를 저장하는 변수
+    private lateinit var fusedLocationClient: FusedLocationProviderClient // 위치 정보 제공
+    private lateinit var locationCallback: LocationCallback // 위치 업데이트 콜백
+    private val handler: Handler // 메인 스레드에서 작업을 예약하기 위한 Handler 객체
+    private var updateInterval: Long = 0L //위치 간격 변수
+    private var lastLocation: Location? = null //제일 최근 위치 저장
 
-    constructor() { // 생성자 // 초기화
+    constructor(context: Context) { // 생성자 // 초기화 context를 받기 위해 추가
         // TODO Location을 받아오는 로직을 불러오기 위해서 초기화하는 로직들 다 함수로 등록
+        this.context = context//conext를 인자로 받아 객체를 생성하는 생성자
+        handler = Handler(Looper.getMainLooper())
+        updateInterval = 3000L
         setupLocationComponents()
     }
 
     fun setLocationCallback(callback: LocationCallback) {
         // TODO Location을 받아오는 callback을 설정한다.
         // startLocationUpdates()를 변형해서 구현
+        locationCallback = callback //콜백을 변수에 저장
+
+        val locationRequest = LocationRequest.create().apply { //LocationRequest 객체 생성, 설정 블록 적용
+            interval = updateInterval // 3초마다 갱신
+            fastestInterval = updateInterval / 2 // 업데이트 주기가 너무 빠르지 않도록 갱신 속도의 절반을 최대 속도로 설정
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY// 고정밀 위치 정보 획득
+        }
+
+        //위치 권한 부여 여부 확인
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            throw SecurityException("Location permission ACCESS_FINE_LOCATION is not granted")// 권한 없으면 메세지 띄워줌
+        }
+
+        fusedLocationClient.requestLocationUpdates(//위치 업데이트 요청
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
     fun getLastLatLng(): Pair<Double, Double> {
         // TODO 마지막 location을 pair로 리턴한다.
-         return Pair(0.0, 0.0)
+        //마지막 위치 정보(위도, 경도)를 반환하며 없으면 0,0 리턴함
+         return lastLocation?.let {Pair(it.latitude, it.longitude)} ?: Pair(0.0, 0.0)
     }
 
     private fun setupLocationComponents() {
         // TODO: Location을 받아오는 client를 초기화한다.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context) // LocationServices를 사용해 context를 기반으로 초기화
+        locationCallback = object : LocationCallback(){ //로케이션 콜백 로직 구현
+            override fun onLocationResult(locationResult: LocationResult?){ //위치 업데이트가 들어올 때 호출되는 콜백 메서드
+                locationResult?.let {//location result가 null이 아닐때 실행
+                    lastLocation = it.lastLocation //최신 위치 갱신
+                }
+            }
+        }
     }
 
 }
