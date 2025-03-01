cadc502e3f60dc3e8ce602abd1386a0055acebb4
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-03-01 22:04:00 +0900

    refactor(NoNetworkActivity.kt)
    네비 엑티비티를 상속하게 변경, 네트워크 연결 끊기면 화면 띄우기

--- app/src/main/java/com/dudoji/android/network/NoNetworkActivity.kt
+++ app/src/main/java/com/dudoji/android/network/NoNetworkActivity.kt
@@ -3,11 +3,22 @@ package com.dudoji.android.network
 import com.dudoji.android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dudoji.android.NavigatableActivity
 
-class NoNetworkActivity : AppCompatActivity(){
+class NoNetworkActivity :  NavigatableActivity(){
+
+    // 네비게이션이 필요 없는 액티비티이므로 빈  mapOf 화면 반환
+    override val navigationItems = mapOf<Int, Class<out AppCompatActivity>?>()
+    override val defaultSelectedItemId = R.id.homeFragment // 기본 값
 
     override fun onCreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
         setContentView(R.layout.activity_network)
+    }
+
+    override fun setupNetworkMonitoring() {
+        networkMonitor.onNetworkAvailable = {
+            finish()
+        }
     }
 }
