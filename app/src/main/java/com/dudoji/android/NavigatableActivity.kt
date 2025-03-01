2c94c19e8fc0f16fea412a7cce3a8a29a1d3978f
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-03-01 22:01:08 +0900

    refactor(NavigatableActivity.kt)
    네비를 이미 상속중이라 일단 여기에 네트워크 관련 기능을 구현함, 이에 대한 방안도 생각해 봐야할듯?

--- app/src/main/java/com/dudoji/android/NavigatableActivity.kt
+++ app/src/main/java/com/dudoji/android/NavigatableActivity.kt
@@ -1,13 +1,19 @@
 package com.dudoji.android
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dudoji.android.network.NetworkMonitor
+import com.dudoji.android.network.NoNetworkActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 abstract class NavigatableActivity : AppCompatActivity() {
 
     // 메뉴 아이템 ID와 대상 Activity 매핑 (대상이 null이면 현재 화면 유지)
     protected abstract val navigationItems: Map<Int, Class<out AppCompatActivity>?>
+
+    //네트워크 모니터 변수 생성
+    protected lateinit var networkMonitor: NetworkMonitor
 
     // 현재 Activity에 해당하는 기본 선택 아이템 ID
     protected abstract val defaultSelectedItemId: Int
@@ -26,14 +32,50 @@ abstract class NavigatableActivity : AppCompatActivity() {
         }
     }
 
+    override  fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        networkMonitor = NetworkMonitor(this)
+        lifecycle.addObserver(networkMonitor)
+    }
+
     override fun onResume() {
         super.onResume()
         updateBottomNavigationSelection()
+        setupNetworkMonitoring()
     }
 
     private fun updateBottomNavigationSelection() {
         findViewById<BottomNavigationView>(R.id.navigationView)?.apply {
             selectedItemId = defaultSelectedItemId
+        }
+    }
+
+    protected open fun setupNetworkMonitoring(){
+        networkMonitor.onNetworkAvailable = {
+            //네트워크가 복구 되었을 때 NoNetworkActivity 끄기
+            if (isNoNetworkActivityVisible()){
+                finishNoNetworkActivity()
+            }
+        }
+
+        networkMonitor.onNetworkLost = {
+            //네트워크가 끊겼을 때 NoNetworkActivity 실행
+            if(!isNoNetworkActivityVisible()){
+                startActivity(Intent(this, NoNetworkActivity::class.java))
+            }
+        }
+    }
+
+    //현재 엑티비티가 NoNetworkActivity인지 확인
+    private fun isNoNetworkActivityVisible(): Boolean {
+        return if(this is NoNetworkActivity) true
+        else false
+    }
+
+    //현재 엑티비티가 NoNetworkActivity이면 finish
+    private fun finishNoNetworkActivity() {
+        if(this is NoNetworkActivity){
+            finish()
         }
     }
 }
