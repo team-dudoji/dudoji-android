4511d55d93c82e26bd6b498f2e14afb9e60cc56f
mandu <jys0972@gmail.com>
2025-03-02 13:28:11 +0900

    feat(MapActivity.kt) : camera position controller 연결

--- app/src/main/java/com/dudoji/android/map/MapActivity.kt
+++ app/src/main/java/com/dudoji/android/map/MapActivity.kt
@@ -4,6 +4,8 @@ import android.location.Location
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import com.dudoji.android.MainActivity
 import com.dudoji.android.NavigatableActivity
 import com.dudoji.android.R
@@ -12,9 +14,10 @@ import com.dudoji.android.config.MIN_ZOOM
 import com.dudoji.android.config.TILE_OVERLAY_LOADING_TIME
 import com.dudoji.android.model.MapSectionManager
 import com.dudoji.android.repository.RevealCircleRepository
-import com.dudoji.android.util.MapUtil
 import com.dudoji.android.util.location.LocationCallbackFilter
 import com.dudoji.android.util.location.LocationService
+import com.dudoji.android.util.map.MapCameraPositionController
+import com.dudoji.android.util.map.MapUtil
 import com.dudoji.android.util.tile.MaskTileProvider
 import com.dudoji.android.util.tile.mask.IMaskTileMaker
 import com.dudoji.android.util.tile.mask.MapSectionMaskTileMaker
@@ -30,6 +33,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.TileOverlay
 import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 
 class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
 
@@ -40,16 +44,19 @@ class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
 
     override val defaultSelectedItemId = R.id.mapFragment
 
+    private lateinit var myLocationButton : Button;
     private lateinit var bottomNav: BottomNavigationView
     private lateinit var locationService: LocationService //로케이션 서비스 변수 추가
 
-    var googleMap: GoogleMap? = null
-    var mapUtil: MapUtil = MapUtil(this)
-    var marker: Marker? = null
+    private var googleMap: GoogleMap? = null
+    private var mapUtil: MapUtil = MapUtil(this)
+    private lateinit var mapCameraPositionController : MapCameraPositionController
 
-    val numOfTileOverlay = 2
-    var indexOfTileOverlay = 0
-    val tileOverlays: MutableList<TileOverlay> = mutableListOf()
+    private var marker: Marker? = null
+
+    private val numOfTileOverlay = 2
+    private var indexOfTileOverlay = 0
+    private val tileOverlays: MutableList<TileOverlay> = mutableListOf()
 
     fun setTileMaskTileMaker(maskTileMaker: IMaskTileMaker) {
         val tileOverlayOptions = TileOverlayOptions().tileProvider(MaskTileProvider(maskTileMaker))
@@ -74,6 +81,7 @@ class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
 
         locationService = LocationService(this)
 
+        setupMyLocationButton()
         setupLocationUpdates() // Setup location updates Callback
     }
 
@@ -87,6 +95,7 @@ class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
                         Log.d("MapActivity", "location callback: Location is Saved")
                         RevealCircleRepository.addLocation(it)
                         updateLocationOnMap(it)
+                        mapCameraPositionController.updateLocation(it)
                     }
                 }
             }
@@ -120,12 +129,14 @@ class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
         }, TILE_OVERLAY_LOADING_TIME)
     }
 
+
     override fun onMapReady(p0: GoogleMap?) {
         googleMap = p0
         mapUtil.setGoogleMap(p0)
         p0?.setMinZoomPreference(MIN_ZOOM)  // set zoom level bounds
         p0?.setMaxZoomPreference(MAX_ZOOM)
-        
+        mapCameraPositionController = MapCameraPositionController(p0!!)
+
         // apply tile overlay to google map
         setTileMaskTileMaker(PositionsMaskTileMaker(
             MapSectionMaskTileMaker(
@@ -134,5 +145,21 @@ class MapActivity :  NavigatableActivity(), OnMapReadyCallback {
                 )
             )
         ))
+    }
+
+    fun setupMyLocationButton() {
+        myLocationButton = findViewById(R.id.myLocationButton)
+        myLocationButton.setOnClickListener {
+            mapCameraPositionController.setAttach(true)
+            myLocationButton.visibility = android.view.View.GONE
+            startLocationUpdates()
+        }
+    }
+
+    private fun startLocationUpdates() {
+        lifecycleScope.launch {
+            while (mapCameraPositionController.isAttached)
+                mapCameraPositionController.update()
+        }
     }
 }
