8b687b12c3718479b295d1179d9110f05cb09088
minseok <micheal117@pusan.ac.kr>
2025-04-26 10:22:37 +0900

    refactor(StatisticsActivity.kt)
    통계 엑티비티 리사이클뷰로 변경

--- app/src/main/java/com/dudoji/android/mypage/StatisticsActivity.kt
+++ app/src/main/java/com/dudoji/android/mypage/StatisticsActivity.kt
@@ -4,19 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dudoji.android.R
+import androidx.recyclerview.widget.RecyclerView
 
 class StatisticsActivity : AppCompatActivity() {
+
+    private lateinit var statsRecyclerView: RecyclerView
+    private lateinit var statisticsAdapter: StatisticsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
 
+        statsRecyclerView = findViewById(R.id.statsRecyclerView)
+
+        // 하드 코딩 박은 데이터
+        val stats = listOf(
+            StatisticItem("총 이동거리", "하루\n5km 주파!"),
+            StatisticItem("오늘 이동 거리", "하루\n3km 주파!"),
+            StatisticItem("최고 속도", "12 km/h"),
+            StatisticItem("최대 이동거리", "10 km")
+        )
+
+        // 리사이클 뷰 항목 배치, LinearLayoutManager는 세로 리스트로 배치한다네요
+        statsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 어댑터 설정
+        statisticsAdapter = StatisticsAdapter(stats) //AchievementAdapter에 하드 코딩 데이터 박음
+        statsRecyclerView.adapter = statisticsAdapter //리사이클뷰에 어뎁터 연결하여 화면 표시 준비 갈 완료
+
+        // 뒤로가기 버튼 스컬~
         val backButton = findViewById<View>(R.id.back_button)
         backButton.setOnClickListener {
             val intent = Intent(this, MypageActivity::class.java)
             startActivity(intent)
-            finish() // 현재 Activity 종료
+            finish()
         }
     }
 }
+
