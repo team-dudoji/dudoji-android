7da8b4de9429e1ce2797665f58f458e4217f5586
minseok <micheal117@pusan.ac.kr>
2025-04-26 17:41:15 +0900

    refactor(StatisticsActivity.kt)
    개인통계 6개 2열 모양 잡기

--- app/src/main/java/com/dudoji/android/mypage/StatisticsActivity.kt
+++ app/src/main/java/com/dudoji/android/mypage/StatisticsActivity.kt
@@ -2,8 +2,10 @@ package com.dudoji.android.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dudoji.android.R
 import androidx.recyclerview.widget.RecyclerView
@@ -19,16 +21,22 @@ class StatisticsActivity : AppCompatActivity() {
 
         statsRecyclerView = findViewById(R.id.statsRecyclerView)
 
-        // 하드 코딩 박은 데이터
+        val categoryTitle = intent.getStringExtra("CATEGORY_TITLE") ?: "없음"
+        Log.d("카테고리 확인", "받은 값: $categoryTitle")
+
         val stats = listOf(
             StatisticItem("총 이동거리", "하루\n5km 주파!"),
             StatisticItem("오늘 이동 거리", "하루\n3km 주파!"),
+            StatisticItem("최대 이동 거리", "10 km"),
             StatisticItem("최고 속도", "12 km/h"),
-            StatisticItem("최대 이동거리", "10 km")
+            StatisticItem("최대 이동 거리", "10 km"),
+            StatisticItem("최고 속도", "12 km/h")
         )
 
-        // 리사이클 뷰 항목 배치, LinearLayoutManager는 세로 리스트로 배치한다네요
-        statsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 리사이클 뷰 항목 배치, 그리드레이웃으로 2열로 만들어버렸
+        val gridLayoutManager = GridLayoutManager(this, 2) // 2열 그리드
+        statsRecyclerView.layoutManager = gridLayoutManager
 
         // 어댑터 설정
         statisticsAdapter = StatisticsAdapter(stats) //AchievementAdapter에 하드 코딩 데이터 박음
