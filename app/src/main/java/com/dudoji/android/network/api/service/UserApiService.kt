0693756d9a593619e48257d89701ea7ef86db7fe
Yunseong <dev.yunseong@gmail.com>
2025-04-26 10:13:35 +0900

    fix(UserApiService.kt) : http Method 수정

--- app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
+++ app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
@@ -2,11 +2,11 @@ package com.dudoji.android.network.api.service
 
 
 import retrofit2.Response
-import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 // User Data API endpoints
 interface UserApiService {
-    @GET("auth/login/kakao/app-login")
+    @POST("auth/login/kakao/app-login")
     suspend fun kakaoLogin(@Header("Authorization") token: String): Response<Void>
 }
