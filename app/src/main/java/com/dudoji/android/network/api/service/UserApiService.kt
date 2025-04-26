2938b2c7d761802d370da5a744e68690484fc08e
Yunseong <dev.yunseong@gmail.com>
2025-04-26 14:51:18 +0900

    feat(TokenResponse) : user api의 response구조 정의

--- app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
+++ app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
@@ -1,6 +1,7 @@
 package com.dudoji.android.network.api.service
 
 
+import com.dudoji.android.network.entity.TokenResponse
 import retrofit2.Response
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -8,5 +9,5 @@ import retrofit2.http.POST
 // User Data API endpoints
 interface UserApiService {
     @POST("auth/login/kakao/app-login")
-    suspend fun kakaoLogin(@Header("Authorization") token: String): Response<Void>
+    suspend fun kakaoLogin(@Header("Authorization") token: String): Response<TokenResponse>
 }
