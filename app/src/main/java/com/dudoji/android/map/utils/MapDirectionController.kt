7132dd940a2f5f76cd0f1600af20ea5156bb1886
minseok <micheal117@pusan.ac.kr>
2025-05-03 15:21:56 +0900

    feature(MapDirectionController.kt)
    화살표 방향 센서 방향에 따라 돌아가게 하는 클래스

---
+++ app/src/main/java/com/dudoji/android/map/utils/MapDirectionController.kt
@@ -0,0 +1,54 @@
+package com.dudoji.android.map.utils
+
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.util.Log
+
+class MapDirectionController(
+    context: Context,
+    private val mapCameraController: MapCameraPositionController,
+    private val locationProvider: () -> Location?
+) : SensorEventListener {
+
+    private val sensorManager =
+        context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    fun start() {
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+
+        if (event.sensor.type != Sensor.TYPE_ROTATION_VECTOR) return
+
+        val rotationMatrix = FloatArray(9)
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+
+        val orientationAngles = FloatArray(3)
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+        val azimuth = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+        val bearing = (azimuth + 360) % 360
+
+        Log.d("MapDirection", "${bearing}")
+
+
+
+        mapCameraController.setBearing(bearing)
+
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+}
