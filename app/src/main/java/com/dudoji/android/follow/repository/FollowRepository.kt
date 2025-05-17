f40b0da0314c452ea210b9cf92258e50a804a29a
Yunseong <dev.yunseong@gmail.com>
2025-05-17 17:30:53 +0900

    feature unfollow 기능 구현

--- app/src/main/java/com/dudoji/android/follow/repository/FollowRepository.kt
+++ app/src/main/java/com/dudoji/android/follow/repository/FollowRepository.kt
@@ -1,6 +1,9 @@
 package com.dudoji.android.follow.repository
 
+import android.R
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import com.dudoji.android.follow.domain.User
 
 object FollowRepository {
@@ -16,11 +19,14 @@ object FollowRepository {
         return followings
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun addFollowing(user: User): Boolean {
         followings.add(user)
         return RetrofitClient.followApiService.addFriend(user.id).isSuccessful
     }
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getRecommendedUsers(email: String): List<User> {
         val response = RetrofitClient.followApiService.getRecommendedUsers(email)
         if (response.isSuccessful) {
@@ -33,6 +39,17 @@ object FollowRepository {
         followings.clear()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun deleteFollowing(user: User): Boolean {
+        val isSuccessful = RetrofitClient.followApiService.deleteFriend(user.id).isSuccessful
+        if (isSuccessful) {
+            followings.remove(user)
+            return true
+        }
+        return false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun loadFollowings() {
         val response = RetrofitClient.followApiService.getFriends()
         if (response.isSuccessful) {
