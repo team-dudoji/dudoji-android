a869647962d3b02f5c87ad38b84b6e6d06a1daa0
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-05-26 00:51:23 +0900

    refactor(MypageActivity.kt)
    네비바 삭제

--- app/src/main/java/com/dudoji/android/mypage/activity/MypageActivity.kt
+++ app/src/main/java/com/dudoji/android/mypage/activity/MypageActivity.kt
@@ -6,37 +6,27 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
-import com.dudoji.android.NavigatableActivity
 import com.dudoji.android.R
 import com.dudoji.android.map.activity.MapActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.imageview.ShapeableImageView
 import kotlinx.coroutines.launch
 
-class MypageActivity : NavigatableActivity() {
+class MypageActivity : AppCompatActivity() {
     private val TAG = "MypageActivityDEBUG"
 
-    override val navigationItems = mapOf(
-        R.id.mypageFragment to null,
-        R.id.mapFragment to MapActivity::class.java
-    )
 
-    private lateinit var bottomNav: BottomNavigationView
 
-    // 기본 선택 항목 설정
-    override val defaultSelectedItemId = R.id.mypageFragment
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
 
-        bottomNav = findViewById(R.id.navigationView)
-        bottomNav.selectedItemId = R.id.mypageFragment
 
-        setupBottomNavigation(findViewById(R.id.navigationView))
 
         setProfile()
 
