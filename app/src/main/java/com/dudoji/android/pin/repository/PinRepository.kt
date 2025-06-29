cadf25f23965550f0451e2bff33027efe06d4c32
minseok <micheal117@pusan.ac.kr>
2025-06-30 00:38:32 +0900

    refactor(PinRepository.kt)
    핀스킨 적용 함수

--- app/src/main/java/com/dudoji/android/pin/repository/PinRepository.kt
+++ app/src/main/java/com/dudoji/android/pin/repository/PinRepository.kt
@@ -6,12 +6,14 @@ import androidx.annotation.RequiresApi
 import com.dudoji.android.config.PIN_UPDATE_THRESHOLD
 import com.dudoji.android.map.utils.MapUtil
 import com.dudoji.android.pin.api.dto.PinRequestDto
+import com.dudoji.android.pin.api.dto.PinSkinUpdateRequestDto
 import com.dudoji.android.pin.domain.Pin
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterManager
 
 object PinRepository {
-    private val pinList = mutableListOf<Pin>()
+    val pinList = mutableListOf<Pin>()
     private lateinit var googleMap: GoogleMap
 
     private var lastPinUpdatedLatLng: LatLng? = null
@@ -60,5 +62,14 @@ object PinRepository {
         } else {
             emptyList()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun updatePinSkin(pinId: Long, newSkin: String): Boolean {
+        val response = RetrofitClient.pinApiService.updatePinSkin(
+            pinId,
+            PinSkinUpdateRequestDto(newSkin)
+        )
+        return response.isSuccessful
     }
 }
