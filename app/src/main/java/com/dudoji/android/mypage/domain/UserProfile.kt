f028f06ee14fa23167766085b570b10b84d3021f
minseok <micheal117@pusan.ac.kr>
2025-07-07 22:32:15 +0900

    feature(UserProfile.kt)
    마이페이지 유저 프로필 관리

---
+++ app/src/main/java/com/dudoji/android/mypage/domain/UserProfile.kt
@@ -0,0 +1,10 @@
+package com.dudoji.android.mypage.domain
+
+data class UserProfile(
+    val name: String,
+    val profileImageUrl: String?,
+    val email: String,
+    val pinCount: Int,
+    val followerCount: Int,
+    val followingCount: Int
+)
