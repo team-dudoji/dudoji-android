1ebf16a276fd0f67322a3b9a569318b5762ff03a
Yunseong Jeong <88422717+dev-yunseong@users.noreply.github.com>
2025-07-07 14:23:36 +0900

    Feature/issue 98 (#100)
    * feature Database를 통해서 MapSection을 다루도록 수정
    
    * feature LocationService를 추상화하고 테스트용 DemoLocationService 구현
    
    * Refactor MapActivity to use DemoLocationService and DatabaseMapSectionManager
    
    * feature fog 걷치는 circle의 가에에 gredient 주기
    
    * feature Update DatabaseMapSectionManager to clear all map sections on initialization and retrieve map sections from the database; increase TILE_SIZE to 512 in MaskTileProvider
    
    * feature Refactor DatabaseMapSectionManager to remove map section deletion on initialization; update MapActivity to use GPSLocationService instead of DemoLocationService

---
+++ app/src/main/java/com/dudoji/android/map/domain/WorldPosition.kt
@@ -0,0 +1,3 @@
+package com.dudoji.android.map.domain
+
+data class WorldPosition(val xOfWold: Double, val yOfWorld: Double, val radius: Int)
--- app/src/main/java/com/dudoji/android/map/manager/MapSectionManager.kt
+++ app/src/main/java/com/dudoji/android/map/manager/MapSectionManager.kt
@@ -3,81 +3,42 @@ package com.dudoji.android.map.manager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.util.Log
 import com.dudoji.android.config.BASIC_ZOOM_LEVEL
 import com.dudoji.android.config.FOG_COLOR
 import com.dudoji.android.map.domain.TileCoordinate
-import com.dudoji.android.map.domain.mapsection.DetailedMapSection
 import com.dudoji.android.map.domain.mapsection.MapSection
 import com.dudoji.android.map.utils.mapsection.BitmapUtil
 import com.dudoji.android.map.utils.mapsection.BitmapUtil.Companion.combineBitmapInGrid
 import com.dudoji.android.map.utils.tile.TILE_SIZE
 
-class MapSectionManager(mapSections: List<MapSection>) {
-    private val mapSections: Map<TileCoordinate, MapSection>
-
-
-    private val emptyBitmap: Bitmap
-    private val fullBitmap: Bitmap
+abstract class MapSectionManager {
+    protected val emptyBitmap: Bitmap
+    protected val fullBitmap: Bitmap
 
     init {
-        this.mapSections = mapSections.associateBy { TileCoordinate(it.x, it.y, 15) }
-        emptyBitmap = BitmapUtil.createBitmapWithColor(TILE_SIZE, TILE_SIZE, Color.TRANSPARENT)
-        fullBitmap = BitmapUtil.createBitmapWithColor(TILE_SIZE, TILE_SIZE, FOG_COLOR)
-    }
-    companion object {
-        private val dirtyMapSectionCoordinates: HashSet<TileCoordinate> = HashSet()
-
-        fun setDirtyCoordinate(coordinate: TileCoordinate) {
-            val diff = BASIC_ZOOM_LEVEL - coordinate.zoom
-
-            val baseX = (coordinate.x shl diff) - 1
-            val baseY = (coordinate.y shl diff) - 1
-            val tileCount = (1 shl kotlin.math.abs(diff)) + 2
-
-            for (x in 0 until tileCount) {
-                for (y in 0 until tileCount) {
-                    val targetCoordinate = TileCoordinate(
-                        baseX + x,
-                        baseY + y,
-                        BASIC_ZOOM_LEVEL
-                    )
-                    Log.d("MapSectionManager", "setDirtyCoordinate: $targetCoordinate")
-                    dirtyMapSectionCoordinates.add(targetCoordinate)
-                }
-            }
-        }
+        emptyBitmap = BitmapUtil.createBitmapWithColor(TILE_SIZE, TILE_SIZE, Color.TRANSPARENT, false)
+        fullBitmap = BitmapUtil.createBitmapWithColor(TILE_SIZE, TILE_SIZE, FOG_COLOR, false)
     }
 
-    fun getDirtyMapSections(): List<MapSection> {
-        val dirtyCoordinates = dirtyMapSectionCoordinates.mapNotNull { coordinate ->
-            mapSections[coordinate] ?: DetailedMapSection(coordinate)
-        }
-        dirtyMapSectionCoordinates.clear()
-        return dirtyCoordinates
-    }
-
+    abstract fun getMapSection(coordinate: TileCoordinate): MapSection?
+    
     fun getBitmap(coordinate: TileCoordinate): Bitmap? {
         if (coordinate.zoom == BASIC_ZOOM_LEVEL) {
-            val mapSection = mapSections[coordinate]
+            val mapSection = getMapSection(coordinate)
             if (mapSection == null) { // MapSection is not found
                 return fullBitmap
             } else { // MapSection is found
-                if (mapSection is DetailedMapSection) {
-                    return mapSection.getBitmap()
-                } else {
-                    return emptyBitmap
-                }
+                mapSection.bitmap ?: emptyBitmap
             }
         } else if (coordinate.zoom < BASIC_ZOOM_LEVEL) {
             return getBitmapWithCombine(coordinate)
         } else if (coordinate.zoom > BASIC_ZOOM_LEVEL) {
             return getBitmapWithCrop(coordinate)
         }
-        return null
+        return fullBitmap
     }
 
-    fun getBitmapWithCombine(coordinate: TileCoordinate): Bitmap { // on zoom level < BASIC_ZOOM_LEVEL
+    private fun getBitmapWithCombine(coordinate: TileCoordinate): Bitmap { // on zoom level < BASIC_ZOOM_LEVEL
         val diffOfZoomLevel = BASIC_ZOOM_LEVEL - coordinate.zoom
         val numOfTile = 1 shl diffOfZoomLevel
         var baseBitmap = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888)
@@ -90,22 +51,19 @@ class MapSectionManager(mapSections: List<MapSection>) {
                     yOfTile + numOfTile * coordinate.y,
                     15
                 )
-                val childMapSection = mapSections[childCoordinate]
+                val childMapSection = getMapSection(childCoordinate)
                 if (childMapSection == null) {
                     canvas.combineBitmapInGrid(fullBitmap, numOfTile, xOfTile, yOfTile)
                 } else {
-                    if (childMapSection is DetailedMapSection) {
-                        canvas.combineBitmapInGrid(childMapSection.getBitmap(), numOfTile, xOfTile, yOfTile)
-                    } else {
-                        canvas.combineBitmapInGrid(emptyBitmap, numOfTile, xOfTile, yOfTile)
-                    }
+                    canvas.combineBitmapInGrid(
+                        childMapSection.bitmap ?: emptyBitmap, numOfTile, xOfTile, yOfTile)
                 }
             }
         }
         return baseBitmap
     }
 
-    fun getBitmapWithCrop(coordinate: TileCoordinate): Bitmap { // on zoom level > BASIC_ZOOM_LEVEL
+    private fun getBitmapWithCrop(coordinate: TileCoordinate): Bitmap { // on zoom level > BASIC_ZOOM_LEVEL
         val diffOfZoomLevel = coordinate.zoom - BASIC_ZOOM_LEVEL
         val numOfTile = 1 shl diffOfZoomLevel
 
@@ -118,13 +76,13 @@ class MapSectionManager(mapSections: List<MapSection>) {
         val xOfTile = coordinate.x - numOfTile * parentTileCoordinate.x
         val yOfTile = coordinate.y - numOfTile * parentTileCoordinate.y
 
-        val parentMapSection = mapSections[parentTileCoordinate]
+        val parentMapSection = getMapSection(parentTileCoordinate)
 
         if (parentMapSection == null) { // Bitmap is not found
             return fullBitmap
         } else { // Bitmap is found
-            if (parentMapSection is DetailedMapSection) {
-                return BitmapUtil.cropBitmapInGrid(parentMapSection.getBitmap(), TILE_SIZE, numOfTile, xOfTile, yOfTile)
+            if (parentMapSection.bitmap != null) {
+                return BitmapUtil.cropBitmapInGrid(parentMapSection.bitmap!!, TILE_SIZE, numOfTile, xOfTile, yOfTile)
             } else {
                 return emptyBitmap
             }
