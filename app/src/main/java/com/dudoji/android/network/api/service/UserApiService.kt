fff3dd7e1c1e8eec6f652d9a20c9dd84a0658a21
minseok <micheal117@pusan.ac.kr>
2025-07-07 22:40:47 +0900

    refactor(UserApiService.kt)
    나머지는 mypagerepository로 이동, getUserProfile 추가

--- app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
+++ app/src/main/java/com/dudoji/android/network/api/service/UserApiService.kt
@@ -1,26 +1,11 @@
 package com.dudoji.android.network.api.service
 
+import com.dudoji.android.mypage.dto.UserProfileDto
 import retrofit2.Response
 import retrofit2.http.GET
 
 // user API endpoints
 interface UserApiService {
-    @GET("/api/user/infos/profile-image")
-    suspend fun getUserProfileImageUrl(): Response<String>
-
-    @GET("/api/user/infos/name")
-    suspend fun getUserName(): Response<String>
-
-    @GET("/api/user/infos/email")
-    suspend fun getUserEmail(): Response<String>
-
-    @GET("/api/user/infos/pinned-count")
-    suspend fun getUserPinnedCount(): Response<Int>
-
-    @GET("/api/user/infos/follower-count")
-    suspend fun getUserFollowerCount(): Response<Int>
-
-    @GET("/api/user/infos/following-count")
-    suspend fun getUserFollowingCount(): Response<Int>
-
+    @GET("/api/user/profiles/mine")
+    suspend fun getUserProfile(): Response<UserProfileDto>
 }
