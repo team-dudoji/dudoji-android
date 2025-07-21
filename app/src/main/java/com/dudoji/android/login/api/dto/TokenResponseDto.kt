5f9c2d2566d2a465a976b5408e353df7bd2a90f4
Yunseong <dev.yunseong@gmail.com>
2025-07-21 10:49:22 +0900

    Add TokenDto and refactor TokenResponseDto to use new data structure

--- app/src/main/java/com/dudoji/android/login/api/dto/TokenResponseDto.kt
+++ app/src/main/java/com/dudoji/android/login/api/dto/TokenResponseDto.kt
@@ -3,8 +3,3 @@ package com.dudoji.android.login.api.dto
 data class TokenResponseDto (
     val token: TokenDto
 )
-
-data class TokenDto (
-    val grantType: String,
-    val accessToken: String
-)
