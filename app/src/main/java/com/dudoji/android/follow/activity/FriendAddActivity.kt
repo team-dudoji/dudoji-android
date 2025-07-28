fa378be0d3132c95b88e873840fb01debaf8aeeb
chaeminseok1234 <micheal117@pusan.ac.kr>
2025-07-28 11:40:51 +0900

    Feature/issue 118 (#119)
    * refactor(FriendAddActivity.kt)
    
    이메일 친구 검색 기능 추가
    
    * refactor(FollowAdapter.kt)
    
    팔로우 팔로잉 기능 수정
    
    * refactor(FollowSeatrchAdapter.kt)
    
    기본 프로필 이미지 변경, item 변경
    
    * refactor(activity_follow_list.xml)
    
    아이템 변경
    
    * refactor(activity_friend_add.xml)
    
    아이템 변경
    
    * refactor(deletable_following_item.xml)
    
    기본 아이콘 이미지로 변경
    
    * refactor(FollowSearchAdapter.kt)
    
    이미지 그대로 뜨도록 null
    
    * refactor(FollowSearchAdapter.kt)
    
    이미지 url 로그 추가

--- app/src/main/java/com/dudoji/android/follow/activity/FriendAddActivity.kt
+++ app/src/main/java/com/dudoji/android/follow/activity/FriendAddActivity.kt
@@ -2,8 +2,9 @@ package com.dudoji.android.follow.activity
 
 import android.os.Build
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.annotation.RequiresApi
@@ -13,60 +14,64 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dudoji.android.R
 import com.dudoji.android.follow.adapter.FollowSearchAdapter
-import com.dudoji.android.follow.domain.User
 import com.dudoji.android.follow.repository.FollowRepository
 import kotlinx.coroutines.launch
 
+@RequiresApi(Build.VERSION_CODES.O)
 class FriendAddActivity : AppCompatActivity() {
 
-    private lateinit var searchInput: EditText
+    private lateinit var searchAdapter: FollowSearchAdapter
+    private lateinit var searchEditText: EditText
     private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: FollowSearchAdapter
+    private lateinit var backButton: ImageButton
 
-        private val recommandUsers = mutableListOf<User>()
-    private val filteredUsers = mutableListOf<User>()
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_add)
 
-        searchInput = findViewById(R.id.friend_search_edit_text)
+        searchEditText = findViewById(R.id.friend_search_edit_text)
         recyclerView = findViewById(R.id.friend_recommend_recycler_view)
+        backButton = findViewById(R.id.back_button)
 
-        val backButton = findViewById<ImageButton>(R.id.back_button)
-        backButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
-
-        adapter = FollowSearchAdapter(filteredUsers, this)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        searchInput.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val query = s.toString()
-                if (query.isNotBlank()) {
-                    fetchRecommendedUsers(query)
-                } else {
-                    recommandUsers.clear()
-                    adapter.notifyDataSetChanged()
-                }
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
+        setupRecyclerView()
+        setupListeners()
     }
 
-    private fun fetchRecommendedUsers(email: String) {
-        lifecycleScope.launch {
-            try {
-                val users = FollowRepository.getRecommendedUsers(email)
-                recommandUsers.clear()
-                recommandUsers.addAll(users)
-                adapter.notifyDataSetChanged()
-            } catch (e: Exception) {
+    private fun setupRecyclerView() {
+        searchAdapter = FollowSearchAdapter(emptyList(), this)
+        recyclerView.adapter = searchAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupListeners() {
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        searchEditText.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                Log.d("skr", "검색 버튼 클릭됨!") // 디버깅 로그
+
+                val email = textView.text.toString().trim()
+                if (email.isNotBlank()) {
+                    searchUserByEmail(email)
+                }
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(textView.windowToken, 0)
+                true
+            } else {
+                false
             }
         }
     }
 
+    private fun searchUserByEmail(email: String) {
+        lifecycleScope.launch {
+            try {
+                val userList = FollowRepository.getRecommendedUsers(email)
+                searchAdapter.updateUserList(userList)
+            } catch (e: Exception) {
+            }
+        }
+    }
 }
