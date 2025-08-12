5723460f6af8335607e328c1340825a203756a91
minseok <micheal117@pusan.ac.kr>
2025-08-12 16:28:37 +0900

    refactor(Landmark.kt)
    Parcelize 사용을 위해 구조 변경

--- app/src/main/java/com/dudoji/android/landmark/domain/Landmark.kt
+++ app/src/main/java/com/dudoji/android/landmark/domain/Landmark.kt
@@ -1,41 +1,23 @@
 package com.dudoji.android.landmark.domain
 
 import android.os.Build
+import android.os.Parcelable
 import androidx.annotation.RequiresApi
 import com.dudoji.android.map.domain.NonClusterMarker
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 
-class Landmark : NonClusterMarker, Serializable {
-    val landmarkId: Long
-    val placeName: String
-    val address: String
-    val content: String
-    val detailImageUrl: String
-    val radius: Double
-    var isDetected: Boolean
+@Parcelize
+@RequiresApi(Build.VERSION_CODES.O)
+data class Landmark(
+    val landmarkId: Long,
+    override val lat: Double,
+    override val lng: Double,
+    val placeName: String,
+    val address: String,
+    val content: String,
+    val mapImageUrl: String,
+    val detailImageUrl: String,
+    val radius: Double,
+    var isDetected: Boolean,
     val hashtags: List<String>
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    constructor(
-        landmarkId: Long,
-        lat: Double,
-        lng: Double,
-        placeName: String,
-        address: String,
-        content: String,
-        mapImageUrl: String,
-        detailImageUrl: String,
-        radius: Double,
-        isDetected: Boolean,
-        hashtags: List<String>
-    ) : super(lat, lng, mapImageUrl) {
-        this.landmarkId = landmarkId
-        this.placeName = placeName
-        this.address = address
-        this.content = content
-        this.detailImageUrl = detailImageUrl
-        this.radius = radius
-        this.isDetected = isDetected
-        this.hashtags = hashtags
-    }
-}
+) : NonClusterMarker(lat, lng, mapImageUrl), Parcelable
