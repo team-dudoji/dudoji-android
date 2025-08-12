0c629643449b6e42f50695cd686089235604aada
minseok <micheal117@pusan.ac.kr>
2025-08-12 16:27:44 +0900

    refactor(build.gradle.kts)
     kotlin-parcelize 플러그인 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
 }
 
 android {
