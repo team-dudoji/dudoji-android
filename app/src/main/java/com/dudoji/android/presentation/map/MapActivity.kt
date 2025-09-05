22dbf96bfc3ee1d20b390d86b5d09cfa144bca89
Yunseong <dev.yunseong@gmail.com>
2025-09-05 14:42:22 +0900

    feature MapActivity.kt, MapViewModel: camera position, bearing 등을 v vm 방식으로 수정

--- app/src/main/java/com/dudoji/android/presentation/map/MapActivity.kt
+++ app/src/main/java/com/dudoji/android/presentation/map/MapActivity.kt
@@ -13,9 +13,12 @@ import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import coil.load
 import com.dudoji.android.R
+import com.dudoji.android.config.DEFAULT_ZOOM_LEVEL
 import com.dudoji.android.config.MAX_ZOOM
 import com.dudoji.android.config.MIN_ZOOM
 import com.dudoji.android.databinding.ActivityMapBinding
@@ -26,11 +29,8 @@ import com.dudoji.android.landmark.domain.Landmark
 import com.dudoji.android.landmark.util.LandmarkApplier
 import com.dudoji.android.map.domain.Npc
 import com.dudoji.android.map.fragment.QuestFragment
-import com.dudoji.android.map.utils.MapCameraPositionController
-import com.dudoji.android.map.utils.MapDirectionController
 import com.dudoji.android.map.utils.MapObject
-import com.dudoji.android.map.utils.location.GPSLocationService
-import com.dudoji.android.map.utils.location.LocationService
+import com.dudoji.android.map.utils.MarkerIconToggler
 import com.dudoji.android.map.utils.npc.NpcApplier
 import com.dudoji.android.map.utils.ui.LandmarkBottomSheet
 import com.dudoji.android.mypage.activity.MyPageActivity
@@ -43,11 +43,18 @@ import com.dudoji.android.presentation.follow.FollowListActivity
 import com.dudoji.android.shop.activity.ShopActivity
 import com.dudoji.android.ui.AnimatedNavButtonHelper
 import com.dudoji.android.util.modal.Modal
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.collections.MarkerManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.IOException
 
@@ -55,10 +62,9 @@ import java.io.IOException
 @RequiresApi(Build.VERSION_CODES.O)
 class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var locationService: LocationService //로케이션 서비스 변수 추가
-    private lateinit var binding: ActivityMapBinding
-
     private val mapViewModel: MapViewModel by viewModels()
+
+    private lateinit var binding: ActivityMapBinding
 
     private val pinApplier: PinApplier by lazy {
         PinApplier(clusterManager, googleMap, this@MapActivity, pinFilter)
@@ -73,22 +79,22 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
     var mapOverlayUI: MapOverlayUI? = null
 
     private lateinit var googleMap: GoogleMap
-    private val mapCameraPositionController : MapCameraPositionController by lazy {
-        MapCameraPositionController(this.googleMap, binding.myLocationButton)
-    }
 
-    lateinit var directionController: MapDirectionController
+    private var dudojiMarker: Marker? = null
+    private var markerIconToggler: MarkerIconToggler? = null
 
     private val clusterManager: ClusterManager<Pin> by lazy {
         ClusterManager(this@MapActivity, googleMap)
     }
+
     private val normalMarkerCollection: MarkerManager.Collection by lazy {
         clusterManager.markerManager.newCollection()
     }
 
     private val pinFilter: PinFilter by lazy {
-        PinFilter(this@MapActivity)
+        PinFilter(binding, this@MapActivity)
     }
+
     private lateinit var landmarkBottomSheet: LandmarkBottomSheet
 
     val activityObjects = mutableListOf<ActivityMapObject>()
@@ -102,19 +108,42 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync(this)
 
-        locationService = GPSLocationService(this)
-
         setupMyLocationButton()
         setupAnimatedNavButtons()
 
         landmarkBottomSheet = LandmarkBottomSheet(findViewById(R.id.landmark_bottom_sheet), this)
 
         setupSearchLandmark()
-    }
 
-    private fun setupLocationUpdates(){
-        locationService.setLocationCallback { location ->
-            mapCameraPositionController.updateLocation(location)
+        lifecycleScope.launch {
+            mapViewModel.landmarkToShow.collect { landmark ->
+                if (landmark == null) return@collect
+                landmarkBottomSheet.open(landmark)
+            }
+        }
+        lifecycleScope.launch {
+            mapViewModel.bearingFlow.collect { bearing ->
+                dudojiMarker?.rotation = bearing
+            }
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mapViewModel.locationFlow.collect { location ->
+                    Log.d("MapActivity", "Received new location: $location")
+                    dudojiMarker?.position = LatLng(location.latitude, location.longitude)
+                    markerIconToggler?.setSpeed(location.speed)
+                    if (::googleMap.isInitialized && mapViewModel.mapUiState.value.isAttached) {
+                        moveTo(location.latitude, location.longitude)
+                    }
+                }
+            }
+        }
+        lifecycleScope.launch {
+            mapViewModel.mapUiState.collect { mapUiState ->
+                Log.d("MapActivity", "Map UI State changed: isAttached=${mapUiState.isAttached}")
+                binding.myLocationButton.visibility =
+                    if (mapUiState.isAttached) View.GONE else View.VISIBLE
+            }
         }
     }
 
@@ -129,37 +158,63 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
         }
 
         binding.myLocationButton.setOnClickListener {
-            mapCameraPositionController.setAttach(true)
-            binding.myLocationButton.visibility = View.GONE
+            mapViewModel.setAttach(true)
         }
     }
 
     // location updating routine
     private fun startLocationUpdates() {
         lifecycleScope.launch {
-            while (true)
-                mapCameraPositionController.update()
+            while (true) {
+                if (::googleMap.isInitialized && mapViewModel.mapUiState.value.isAttached) {
+                    moveTo(mapViewModel.locationFlow.value.latitude, mapViewModel.locationFlow.value.longitude)
+                }
+                delay(100)
+            }
         }
     }
 
     override fun onPause() {
         super.onPause()
-        if (::directionController.isInitialized) {
-            directionController.stop()
+    }
+
+    fun setupDudojiMarker() {
+        if (dudojiMarker == null) {
+            val markerIcon = BitmapDescriptorFactory.fromResource(R.drawable.marker_main)
+            dudojiMarker = googleMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(0.0, 0.0))
+                    .icon(markerIcon)
+                    .anchor(0.5f, 0.5f)
+                    .flat(true)
+            )
+
+            markerIconToggler = MarkerIconToggler(dudojiMarker!!)
         }
+    }
+
+    fun updateLocationToViewModel() {
+        Log.d("MapActivity", "Updating location to ViewModel")
+        val centerLatLng = googleMap.cameraPosition.target
+        mapViewModel.updateCenterLocation(centerLatLng)
     }
 
     fun moveTo(lat: Double, lng: Double) {
         if (!::googleMap.isInitialized) return
-        mapCameraPositionController.moveCameraPosition(lat, lng, 15f)
+        val latLng = LatLng(lat, lng)
+        val position: CameraPosition = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(DEFAULT_ZOOM_LEVEL)
+            .build()
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(position))
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
-        setupLocationUpdates() // Setup location updates Callback
-        this.googleMap.setMinZoomPreference(MIN_ZOOM)
-        this.googleMap.setMaxZoomPreference(MAX_ZOOM)
-        mapCameraPositionController
+        googleMap.setMinZoomPreference(MIN_ZOOM)
+        googleMap.setMaxZoomPreference(MAX_ZOOM)
+
+        setupDudojiMarker()
 
         lifecycleScope.launch {
             startLocationUpdates()
@@ -172,6 +227,8 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
             )
 
             googleMap.setOnCameraIdleListener {
+                updateLocationToViewModel()
+                mapViewModel.onCameraIdle()
                 clusterManager.onCameraIdle()
                 pinApplier.onCameraIdle()
                 landmarkApplier.onCameraIdle()
@@ -179,6 +236,7 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
             }
 
             googleMap.setOnCameraMoveListener {
+                updateLocationToViewModel()
                 updateMapObjects()
             }
 
@@ -190,7 +248,7 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
                 val tag = marker.tag
                 if (tag is Landmark) {
                     lifecycleScope.launch {
-                        landmarkBottomSheet.open(tag)
+
                     }
                     true
                 } else if (tag is Npc) {
@@ -201,13 +259,9 @@ class MapActivity :  AppCompatActivity(), OnMapReadyCallback {
                     )
                     true
                 }
-
                 false
             }
         }
-
-        directionController = MapDirectionController(this@MapActivity, mapCameraPositionController)
-        directionController.start()
 
         mapOverlayUI = MapOverlayUI(binding, this, assets, googleMap, pinApplier, clusterManager)
     }
--- app/src/main/java/com/dudoji/android/presentation/map/MapViewModel.kt
+++ app/src/main/java/com/dudoji/android/presentation/map/MapViewModel.kt
@@ -1,8 +1,95 @@
 package com.dudoji.android.presentation.map
 
+import android.location.Location
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dudoji.android.domain.usecase.MapUseCase
+import com.dudoji.android.landmark.domain.Landmark
+import com.dudoji.android.map.domain.Npc
+import com.dudoji.android.pin.repository.PinRepository
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.math.abs
 
-//@HiltViewModel
-class MapViewModel: ViewModel() {
+@HiltViewModel
+@RequiresApi(Build.VERSION_CODES.O)
+class MapViewModel @Inject constructor(
+    mapUseCase: MapUseCase
+) : ViewModel(), GoogleMap.OnCameraIdleListener {
 
+    private val _mapUiState = MutableStateFlow(MapUiState())
+    val mapUiState = _mapUiState
+
+    private val _landmarkToShow = MutableStateFlow<Landmark?>(null)
+    val landmarkToShow: StateFlow<Landmark?> = _landmarkToShow
+
+    val locationFlow: StateFlow<Location> = mapUseCase.getLocationUpdates()
+    val bearingFlow: StateFlow<Float> = mapUseCase.getBearing()
+
+    init {
+        viewModelScope.launch {
+            locationFlow.collect {
+                    location ->
+                Log.d("MapViewModel", "New location: $location")
+                if (_mapUiState.value.isAttached) {
+
+                }
+            }
+        }
+    }
+
+    private val _centerLocation = MutableStateFlow<LatLng>(LatLng(0.0, 0.0))
+    val centerLocation: StateFlow<LatLng> = _centerLocation
+
+    fun updateCenterLocation(latLng: LatLng) {
+        _centerLocation.value = latLng
+
+        if (abs(locationFlow.value.latitude - _centerLocation.value.latitude) < 0.0001 &&
+            abs(locationFlow.value.longitude - _centerLocation.value.longitude) < 0.0001) {
+            setAttach(true)
+        } else {
+            setAttach(false)
+        }
+    }
+
+    fun setAttach(isAttached: Boolean) {
+        Log.d("MapViewModel", "setAttach: $isAttached")
+        _mapUiState.value = _mapUiState.value.copy(
+            isAttached = isAttached
+        )
+    }
+
+    fun loadPin() {
+//        PinRepository.load()
+        PinRepository.getPins()
+    }
+
+    fun loadLandmark() {
+
+    }
+
+    fun loadNpc() {
+
+    }
+
+    override fun onCameraIdle() {
+
+    }
 }
+
+data class MapUiState (
+    var isLoading: Boolean = false,
+    var errorMessage: String? = null,
+    var isAttached: Boolean = true,
+    var pins: List<Npc> = emptyList(),
+    var landmarks: List<Npc> = emptyList(),
+    var npcs: List<Npc> = emptyList()
+)
