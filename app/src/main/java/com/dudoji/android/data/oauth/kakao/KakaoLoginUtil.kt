ccff649a7dd833805bcb7b26f88723e2f598ee4e
minseok <micheal117@pusan.ac.kr>
2025-09-06 19:47:16 +0900

    refactor(KakaoLoginUtil.kt)
    기능은 LoginViewModel로 넘기고 로그인 토큰만 받아오는 역할로 변경

--- app/src/main/java/com/dudoji/android/data/oauth/kakao/KakaoLoginUtil.kt
+++ app/src/main/java/com/dudoji/android/data/oauth/kakao/KakaoLoginUtil.kt
@@ -1,101 +1,51 @@
 package com.dudoji.android.data.oauth.kakao
 
 import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
-import com.dudoji.android.presentation.util.getEncryptedPrefs
-import com.dudoji.android.presentation.map.MapActivity
-import com.dudoji.android.network.NetworkInitializer
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
 
 object KakaoLoginUtil {
     val TAG = "KakaoLoginUtilDEBUG"
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private val userApiService = RetrofitClient.loginApiService
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    val callback: (OAuthToken?, Throwable?, Context) -> Unit = { token, error, context ->
-        if (error != null) {
-            Log.e(TAG, "Unsuccess to kakao login", error)
-        } else if (token != null) {
-            CoroutineScope(Dispatchers.Main).launch {
-                onLoginSuccess(token, context)
-            }
+    fun tryLoginWithKakao(
+        context: Context,
+        onResult: (token: OAuthToken?, error: Throwable?) -> Unit
+    ) {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            onResult(token, error)
         }
-    }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    suspend fun onLoginSuccess(token: OAuthToken, context: Context) {
-        Log.d(TAG, "Success to Kakao login")
-        val response = userApiService.kakaoLogin(token.accessToken)
-
-        if (response.isSuccessful) {
-            Log.d(TAG, "Success to dudoji login ${response.body()}")
-            val accessToken = response.body()?.token?.accessToken
-
-            val prefs = getEncryptedPrefs(context)
-            prefs.edit().putString("jwt", accessToken).apply()
-
-            NetworkInitializer.initAuthed(context)
-
-            val intent = Intent(context, MapActivity::class.java)
-            context.startActivity(intent)
-        } else {
-            Log.e(TAG, "Unsuccess to dudoji login ${response.code()}")
-
-            // for debug =====================================
-            NetworkInitializer.initAuthed(context)
-            val intent = Intent(context, MapActivity::class.java)
-            context.startActivity(intent)
-            // ============================================
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun tryLoginWithKakao(context: Context) {
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            loginWithKakaoTalk(context)
+            loginWithKakaoTalk(context, callback)
         } else {
-            loginWithKakaoAccount(context)
+            loginWithKakaoAccount(context, callback)
         }
     }
 
-    fun loginWithKakaoAccount(context: Context) {
-        UserApiClient.instance.loginWithKakaoAccount(context) {
-                token, error ->
-            callback(token, error, context)
-        }
+    private fun loginWithKakaoAccount(
+        context: Context,
+        callback: (OAuthToken?, Throwable?) -> Unit
+    ) {
+        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun loginWithKakaoTalk(context: Context) {
+    private fun loginWithKakaoTalk(
+        context: Context,
+        callback: (OAuthToken?, Throwable?) -> Unit
+    ) {
         UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
             if (error != null) {
-                Log.e(TAG, "Unsuccess to login with Kakao Talk ", error)
-
+                Log.e(TAG, "카카오톡으로 로그인 실패", error)
                 if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    callback(null, error)
                     return@loginWithKakaoTalk
                 }
-
-                loginWithKakaoAccount(context)
-            }
-
-            if (token == null) {
-                return@loginWithKakaoTalk
-            }
-
-            CoroutineScope(Dispatchers.Main).launch {
-                onLoginSuccess(token, context)
+                loginWithKakaoAccount(context, callback)
+            } else if (token != null) {
+                callback(token, null)
             }
         }
     }
