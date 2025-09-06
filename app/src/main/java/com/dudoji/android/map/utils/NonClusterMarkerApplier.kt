2e5f141d564562f9c4718879d7f5f3025f9e2561
Yunseong <dev.yunseong@gmail.com>
2025-09-06 12:58:19 +0900

    feature: refactor LandmarkApplier and NonClusterMarkerApplier to use Context instead of MapActivity

--- app/src/main/java/com/dudoji/android/map/utils/NonClusterMarkerApplier.kt
+++ app/src/main/java/com/dudoji/android/map/utils/NonClusterMarkerApplier.kt
@@ -1,34 +1,28 @@
 package com.dudoji.android.map.utils
 
+import RetrofitClient
+import android.content.Context
 import android.graphics.Bitmap
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.graphics.drawable.toBitmap
-import androidx.lifecycle.lifecycleScope
 import coil.request.ImageRequest
 import coil.request.SuccessResult
-import com.dudoji.android.presentation.map.MapActivity
 import com.dudoji.android.map.domain.NonClusterMarker
 import com.dudoji.android.network.Coil
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.collections.MarkerManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.collections.forEach
 
+@RequiresApi(Build.VERSION_CODES.O)
 abstract class NonClusterMarkerApplier<T : NonClusterMarker> (
     val normalMarkerCollection: MarkerManager.Collection,
-    val googleMap: GoogleMap,
-    val activity: MapActivity): OnCameraIdleListener {
-    private var hasToReload = true
-    private val appliedMarkerBases: HashSet<T> = HashSet()
+    val context: Context
+) {
 
     protected var isIncludedBaseUrl = false
 
@@ -36,28 +30,23 @@ abstract class NonClusterMarkerApplier<T : NonClusterMarker> (
         const val TAG = "NonClusterMarkerApplier"
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun applyMarker(markerBases: List<T>) {
         markerBases.forEach { markerBase ->
-            if (!appliedMarkerBases.contains(markerBase)) {
-                val marker = normalMarkerCollection.addMarker(markerBase.toMarkerOptions())
-                marker?.tag = markerBase
-                appliedMarkerBases.add(markerBase)
-                applyMarkerIcon(marker, markerBase)
-                onMarkerLoaded(marker)
-            }
+            val marker = normalMarkerCollection.addMarker(markerBase.toMarkerOptions())
+            marker?.tag = markerBase
+            applyMarkerIcon(marker, markerBase)
+            onMarkerLoaded(marker)
         }
     }
 
     open fun onMarkerLoaded(marker: Marker?) {}
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun applyMarkerIcon(marker: Marker?, markerBase: T) {
         if (marker == null) return
 
         CoroutineScope(Dispatchers.IO).launch {
             val loader = Coil.imageLoader
-            val request = ImageRequest.Builder(activity)
+            val request = ImageRequest.Builder(context)
                 .data(if (isIncludedBaseUrl) {markerBase.iconUrl} else {"${RetrofitClient.BASE_URL}/${markerBase.iconUrl}"})
                 .build()
 
@@ -73,29 +62,11 @@ abstract class NonClusterMarkerApplier<T : NonClusterMarker> (
         }
     }
 
-    fun markForReload() {
-        hasToReload = true
-    }
-
-    fun clearLandmarks() {
-        appliedMarkerBases.clear()
+    fun clear() {
         normalMarkerCollection.clear()
     }
 
-    abstract suspend fun load(latLng: LatLng, radius: Double): Boolean
-    abstract fun getMarkerBases(): List<T>
+    fun add(markers: List<T>) {
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCameraIdle() {
-        activity.lifecycleScope.launch {
-            if (hasToReload || load(
-                    googleMap.projection.visibleRegion.latLngBounds.center,
-                    100.0)) {
-                val markerBases = getMarkerBases()
-                Log.d(TAG, "Loaded landmarks: ${markerBases.size}")
-                applyMarker(markerBases)
-            }
-            hasToReload = false
-        }
     }
 }
--- app/src/main/java/com/dudoji/android/map/utils/npc/NpcApplier.kt
+++ app/src/main/java/com/dudoji/android/map/utils/npc/NpcApplier.kt
@@ -1,49 +1,27 @@
 package com.dudoji.android.map.utils.npc
 
-import android.graphics.BitmapFactory
+import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.dudoji.android.R
-import com.dudoji.android.domain.model.ActivityMapObject
 import com.dudoji.android.map.domain.Npc
-import com.dudoji.android.map.repository.NpcDataSource
 import com.dudoji.android.map.utils.NonClusterMarkerApplier
-import com.dudoji.android.presentation.map.MapActivity
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.collections.MarkerManager
 
-class NpcApplier(normalMarkerCollection: MarkerManager.Collection, googleMap: GoogleMap, activity: MapActivity)
-    : NonClusterMarkerApplier<Npc>(normalMarkerCollection, googleMap, activity), OnCameraIdleListener{
+@RequiresApi(Build.VERSION_CODES.O)
+class NpcApplier(
+    normalMarkerCollection: MarkerManager.Collection,
+    context: Context,
+    val onNpcLoaded: (Npc) -> Unit,
+    ): NonClusterMarkerApplier<Npc>(normalMarkerCollection, context) {
 
     init {
         isIncludedBaseUrl = true
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override suspend fun load(
-        latLng: LatLng,
-        radius: Double
-    ): Boolean {
-        return NpcDataSource.load(latLng, radius)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun getMarkerBases(): List<Npc> {
-        return NpcDataSource.getNpcs()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onMarkerLoaded(marker: Marker?) {
         val npc = marker?.tag as? Npc ?: return
-        if (npc.hasQuest) {
-            val initialBitmap = BitmapFactory.decodeResource(activity.resources, R.mipmap.quest_bubble)
-            val activityMapObject =
-                ActivityMapObject(LatLng(npc.lat, npc.lng), initialBitmap, 70f, -150f, 170, -100)
-            npc.activityMapObject = activityMapObject
-            activity.activityObjects.add(activityMapObject)
-        }
+        onNpcLoaded(npc)
     }
 }
