2e999f1e3eb4d2e735d64272c845ad0cd0253ebd
minseok <micheal117@pusan.ac.kr>
2025-09-06 19:53:41 +0900

    refactor(SplashActivity.kt)
    viewmodel로 기능 이전

--- app/src/main/java/com/dudoji/android/presentation/login/activity/SplashActivity.kt
+++ app/src/main/java/com/dudoji/android/presentation/login/activity/SplashActivity.kt
@@ -3,61 +3,35 @@ package com.dudoji.android.presentation.login.activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.dudoji.android.R
-import com.dudoji.android.presentation.util.getEncryptedPrefs
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.security.GeneralSecurityException
+import com.dudoji.android.presentation.login.viewmodel.SplashViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 class SplashActivity : AppCompatActivity() {
 
-    private val PREFS_FILENAME = "secret_prefs"
+    private lateinit var viewModel: SplashViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+
         val logoImage = findViewById<ImageView>(R.id.splash_logo_image)
         val dudojiImage = findViewById<ImageView>(R.id.dudoji_image)
-
         logoImage.load("file:///android_asset/splash/splash_logo.png")
         dudojiImage.load("file:///android_asset/splash/dudoji.png")
 
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
-
-            var jwt: String? = null
-            try {
-                val prefs = getEncryptedPrefs(this@SplashActivity)
-                jwt = prefs.getString("jwt", null)
-
-            } catch (e: GeneralSecurityException) {
-                Log.e("ENCRYPTION_ERROR", "암호화된 SharedPreferences 로딩 실패. 파일을 삭제하고 다시 시도합니다.", e)
-
-                deleteSharedPreferences(PREFS_FILENAME)
-
-                jwt = null
-
-            } catch (e: Exception) {
-                Log.e("SPLASH_ERROR", "알 수 없는 오류 발생", e)
-                jwt = null
-            }
-
-            if (jwt.isNullOrEmpty()) {
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-            } else {
-                startActivity(Intent(this@SplashActivity, AutoLoginActivity::class.java))
-            }
-
+        viewModel.navigationTarget.observe(this) { destinationActivity ->
+            startActivity(Intent(this, destinationActivity))
             finish()
         }
+
+        viewModel.decideNextActivity()
     }
 }
