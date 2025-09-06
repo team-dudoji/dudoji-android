b8e3d6b01428efbcab3d30a31a5a798fcfb36006
minseok <micheal117@pusan.ac.kr>
2025-09-06 19:52:36 +0900

    refactor(AutoLoginActivity.kt)
    기존 기능 AutoLoginViewModel로 이동

--- app/src/main/java/com/dudoji/android/presentation/login/activity/AutoLoginActivity.kt
+++ app/src/main/java/com/dudoji/android/presentation/login/activity/AutoLoginActivity.kt
@@ -1,66 +1,32 @@
 package com.dudoji.android.presentation.login.activity
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.dudoji.android.R
-import com.dudoji.android.presentation.util.getEncryptedPrefs
-import com.dudoji.android.map.activity.MapActivity
-import com.dudoji.android.network.NetworkInitializer
-import com.dudoji.android.network.utils.NoNetWorkUtil
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import android.widget.ImageView
+import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.dudoji.android.R
+import com.dudoji.android.presentation.login.viewmodel.AutoLoginViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 class AutoLoginActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    private lateinit var viewModel: AutoLoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auto_login)
+        viewModel = ViewModelProvider(this).get(AutoLoginViewModel::class.java)
 
         findViewById<ImageView>(R.id.imageView).load("file:///android_asset/login/login_logo.png")
 
-        NoNetWorkUtil(this).checkNetworkAndNavigate()
-
-        NetworkInitializer.initNonAuthed(this)
-
-        CoroutineScope(Dispatchers.Main).launch {
-            tryAutoLogin()
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private suspend fun tryAutoLogin() {
-        val prefs = getEncryptedPrefs(this)
-        val jwt = prefs.getString("jwt", null)
-        if (jwt.isNullOrEmpty()) {
-            navigateToLogin()
-            return
+        viewModel.navigationTarget.observe(this) { intent ->
+            startActivity(intent)
+            finish()
         }
 
-        try {
-            val response = RetrofitClient.loginApiService.validateJwt("Bearer $jwt")
-            if (response.isSuccessful) {
-                NetworkInitializer.initAuthed(this)
-                startActivity(Intent(this, MapActivity::class.java))
-                finish()
-            } else {
-                Log.e("JWT", "Invalid JWT: ${response.code()}")
-                navigateToLogin()
-            }
-        } catch (e: Exception) {
-            Log.e("JWT", "Network error: ${e.message}")
-            navigateToLogin()
-        }
-    }
-
-    private fun navigateToLogin() {
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+        viewModel.validateToken()
     }
 }
